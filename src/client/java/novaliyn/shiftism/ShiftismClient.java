3ce2248fddc0e078bb0aff0391eac7c920eb3d4b
Nova <189510880+Novaliyn@users.noreply.github.com>
2026-03-11 13:47:25 -0700

    convert from kotlin to java

---
+++ src/client/java/novaliyn/shiftism/ShiftismClient.java
@@ -0,0 +1,10 @@
+package novaliyn.shiftism;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShiftismClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
--- src/client/kotlin/novaliyn/shiftism/ShiftismClient.kt
+++
@@ -1,9 +0,0 @@
-package novaliyn.shiftism
-
-import net.fabricmc.api.ClientModInitializer
-
-object ShiftismClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
