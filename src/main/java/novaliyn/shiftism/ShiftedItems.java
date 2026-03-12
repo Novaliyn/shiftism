534bd24ecf391fdda7616f4c038cf0bf78b958b0
Nova <189510880+Novaliyn@users.noreply.github.com>
2026-03-11 17:48:41 -0700

    yes

--- src/main/java/novaliyn/shiftism/ShiftedItems.java
+++ src/main/java/novaliyn/shiftism/ShiftedItems.java
@@ -1,21 +1,21 @@
 package novaliyn.shiftism;
 
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.component.*;
+import net.minecraft.world.item.consume_effects.ApplyStatusEffectsConsumeEffect;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.*;
 import net.minecraft.resources.Identifier;
+
 import java.util.function.Function;
 
 public class ShiftedItems {
-	public static void initialize() {
-		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES)
-		.register((itemGroup) -> itemGroup.accept(ShiftedItems.shiftedTestObject));
-	}
-
 	public static <T extends Item> T register(String name, Function<Item.Properties, T> itemFactory, Item.Properties settings) {
 		// Create the item key.
 		ResourceKey<Item> itemKey = ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(Shiftism.MOD_ID, name));
@@ -25,7 +25,17 @@ public class ShiftedItems {
 		Registry.register(BuiltInRegistries.ITEM, itemKey, item);
 		return item;
 	}
-	
-	//Generic Items
-	public static final Item shiftedTestObject = register("shifted_test_object", Item::new, new Item.Properties());
+
+	//Properties
+	public static final FoodProperties ALWAYS_EAT = new FoodProperties.Builder().alwaysEdible().build();
+	public static final Consumable SPEED_JUMP = Consumables.defaultFood().onConsume(new ApplyStatusEffectsConsumeEffect(new MobEffectInstance(MobEffects.SPEED, 30 * 20, 1), 1.0f)).build();
+
+
+	//Registering Items
+	public static Item shiftedTestObject;
+	public static Block shiftedBrewingStand;
+
+	public static void initialize() {
+		shiftedTestObject = register("shifted_test_object", Item::new, new Item.Properties().food(ALWAYS_EAT, SPEED_JUMP));
+	}
 }
