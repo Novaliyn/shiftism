b4bb1e1ef0be8db0d3750d74cea4a58c4475f71c
Nova <189510880+Novaliyn@users.noreply.github.com>
2026-03-12 13:29:27 -0700

    Custom Creative Tab

--- src/main/java/com/novaliyn/shiftism/ShiftedItems.java
+++ src/main/java/com/novaliyn/shiftism/ShiftedItems.java
@@ -1,8 +1,11 @@
 package com.novaliyn.shiftism;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -10,7 +13,6 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.component.*;
 import net.minecraft.world.item.consume_effects.ApplyStatusEffectsConsumeEffect;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.*;
 import net.minecraft.resources.Identifier;
 
@@ -25,6 +27,25 @@ public class ShiftedItems {
 		// Register the item.
 		Registry.register(BuiltInRegistries.ITEM, itemKey, item);
 		return item;
+	}
+
+	public static final ResourceKey<CreativeModeTab> CUSTOM_CREATIVE_TAB_KEY = ResourceKey.create(BuiltInRegistries.CREATIVE_MODE_TAB.key(), Identifier.fromNamespaceAndPath(Shiftism.MOD_ID, "creative_tab"));
+	public static final CreativeModeTab CUSTOM_CREATIVE_TAB = FabricItemGroup.builder()
+			.icon(() -> new ItemStack(ShiftedItems.miku_sword))
+			.title(Component.translatable("itemGroup.shiftism"))
+			.displayItems((params, output) -> {
+				//Items
+				output.accept(ShiftedItems.miku_ingot);
+				output.accept(ShiftedItems.miku_sword);
+
+				//Blocks
+				output.accept(ShiftedBlocks.miku_block);
+			})
+			.build();
+
+	static {
+		// Register the group.
+		Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, CUSTOM_CREATIVE_TAB_KEY, CUSTOM_CREATIVE_TAB);
 	}
 
 	//Properties
