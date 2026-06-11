ac88ebd1e38764c042cb89b58316e240e821b505
novaliyn <189510880+Novaliyn@users.noreply.github.com>
2026-06-11 12:10:25 -0700

    fixed basic enchantments. version 0.1.3 snapshot 1

--- src/client/java/com/novaliyn/shiftism/ShiftismTagsProvider.java
+++ src/client/java/com/novaliyn/shiftism/ShiftismTagsProvider.java
@@ -5,6 +5,7 @@ import java.util.concurrent.CompletableFuture;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.tags.ItemTags;
 
 public class ShiftismTagsProvider extends FabricTagsProvider.ItemTagsProvider {
 	public ShiftismTagsProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
@@ -14,5 +15,33 @@ public class ShiftismTagsProvider extends FabricTagsProvider.ItemTagsProvider {
 	@Override
 	protected void addTags(HolderLookup.Provider wrapperLookup) {
 			valueLookupBuilder(ShiftedItems.SHIFTED_REPAIR).add(ShiftedItems.shifted_ingot).setReplace(true);
+			//helmet
+			valueLookupBuilder(ItemTags.DURABILITY_ENCHANTABLE).add(ShiftedItems.shifted_helmet);
+			valueLookupBuilder(ItemTags.ARMOR_ENCHANTABLE).add(ShiftedItems.shifted_helmet);
+			valueLookupBuilder(ItemTags.HEAD_ARMOR).add(ShiftedItems.shifted_helmet);
+			valueLookupBuilder(ItemTags.HEAD_ARMOR_ENCHANTABLE).add(ShiftedItems.shifted_helmet);
+			//chestplate
+			valueLookupBuilder(ItemTags.DURABILITY_ENCHANTABLE).add(ShiftedItems.shifted_chestplate);
+			valueLookupBuilder(ItemTags.ARMOR_ENCHANTABLE).add(ShiftedItems.shifted_chestplate);
+			valueLookupBuilder(ItemTags.CHEST_ARMOR).add(ShiftedItems.shifted_chestplate);
+			valueLookupBuilder(ItemTags.CHEST_ARMOR_ENCHANTABLE).add(ShiftedItems.shifted_chestplate);
+			//leggings
+			valueLookupBuilder(ItemTags.DURABILITY_ENCHANTABLE).add(ShiftedItems.shifted_leggings);
+			valueLookupBuilder(ItemTags.ARMOR_ENCHANTABLE).add(ShiftedItems.shifted_leggings);
+			valueLookupBuilder(ItemTags.LEG_ARMOR).add(ShiftedItems.shifted_leggings);
+			valueLookupBuilder(ItemTags.LEG_ARMOR_ENCHANTABLE).add(ShiftedItems.shifted_leggings);
+			//boots
+			valueLookupBuilder(ItemTags.DURABILITY_ENCHANTABLE).add(ShiftedItems.shifted_boots);
+			valueLookupBuilder(ItemTags.ARMOR_ENCHANTABLE).add(ShiftedItems.shifted_boots);
+			valueLookupBuilder(ItemTags.FOOT_ARMOR).add(ShiftedItems.shifted_boots);
+			valueLookupBuilder(ItemTags.FOOT_ARMOR_ENCHANTABLE).add(ShiftedItems.shifted_boots);
+			//pickaxe
+			valueLookupBuilder(ItemTags.DURABILITY_ENCHANTABLE).add(ShiftedItems.shifted_pickaxe);
+			valueLookupBuilder(ItemTags.MINING_ENCHANTABLE).add(ShiftedItems.shifted_pickaxe);
+			valueLookupBuilder(ItemTags.PICKAXES).add(ShiftedItems.shifted_pickaxe);
+			//sword
+			valueLookupBuilder(ItemTags.DURABILITY_ENCHANTABLE).add(ShiftedItems.shifted_sword);
+			valueLookupBuilder(ItemTags.WEAPON_ENCHANTABLE).add(ShiftedItems.shifted_sword);
+			valueLookupBuilder(ItemTags.SWORDS).add(ShiftedItems.shifted_sword);
 	}
 }
